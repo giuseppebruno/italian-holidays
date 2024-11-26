d3c7fc3feb47c46030f32f2ab408492d70d6aa26
Giuseppe Bruno <info@gbrunodev.it>
2024-11-26 19:24:05 +0100

    docs: update pyproject.toml to include new classifiers and version feat(pyproject.toml): add italian-holidays project metadata build(setuptools): update setup.py to use correct version number ci(setuptools): update setup.py to include new python versions style(setuptools): format code in setup.py for readability

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='italian-holidays',
-    version='0.0.4',
+    version='0.0.5',
     author='Giuseppe Bruno',
     author_email='info@gbrunodev.it',
     description='A simple Python helper class to determine Italian holidays with the possibility to set custom holidays.',
@@ -22,6 +22,9 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
